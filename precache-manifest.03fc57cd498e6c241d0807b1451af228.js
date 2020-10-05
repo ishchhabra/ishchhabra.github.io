2525a443e7549401b2815f5b967b7900733f0318
ishchhabra <ishchhabra12@gmail.com>
2020-10-05 21:58:41 +0530

    Updates

---
+++ precache-manifest.03fc57cd498e6c241d0807b1451af228.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "870d27896978c4be535b",
+    "url": "/static/css/main.4b78507b.chunk.css"
+  },
+  {
+    "revision": "870d27896978c4be535b",
+    "url": "/static/js/main.870d2789.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "abeab492533c9654ef50791a09c7b22e",
+    "url": "/index.html"
+  }
+];
