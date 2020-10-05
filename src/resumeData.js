49c1c60d05387c8b6ff7175ddbe1cfbffdc54323
ishchhabra <ishchhabra12@gmail.com>
2020-10-05 22:20:25 +0530

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,98 @@
+let resumeData = {
+  imagebaseurl: "https://ishchhabra.github.io/",
+  name: "Ish Chhabra",
+  description:
+    "I am an honors sophomore studying at University of Massachusetts Amherst graduating in Fall 2023.",
+  linkedinId: "ishchhabra",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/ishchhabra/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "http://github.com/ishchhabra",
+      className: "fa fa-github",
+    },
+  ],
+  aboutme:
+    "I am currently an honors sophomore majoring in Computer Science at UMass Amherst.",
+  address: "India",
+  website: "http://github.com/ishchhabra",
+  education: [
+    {
+      UniversityName: "University of Massachusetts Amherst",
+      specialization: "Computer Science",
+      MonthOfPassing: "May",
+      YearOfPassing: "2023",
+      Achievements: ["Dean's List Fall '19", "Dean's List Spring '20"],
+    },
+  ],
+  work: [
+    {
+      CompanyName: "CICS, UMass",
+      specialization: "Full Stack Developer (Intern)",
+      MonthOfJoining: "Jun",
+      YearOfJoining: "2020",
+      MonthOfLeaving: "Aug",
+      YearOfLeaving: "2020",
+      Achievements: "Some Achievements",
+    },
+    {
+      CompanyName: "Hungry Stabbers Gaming",
+      specialization: "Software Developer/Co-Founder",
+      MonthOfJoining: "Aug",
+      YearOfJoining: "2017",
+      MonthOfLeaving: "Mar",
+      YearOfLeaving: "2019",
+      Achievements: "Some Achievements",
+    },
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "Reactjs",
+    },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+  ],
+};
+
+export default resumeData;
