a52d2f0394ff43780dee1ef46a4fc009fb08f2ba
Ish Chhabra <ichhabra@umass.edu>
2021-11-05 02:45:37 -0400

    Update resume on website

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,8 +1,7 @@
 let resumeData = {
   imagebaseurl: "https://ishchhabra.github.io/",
   name: "Ish Chhabra",
-  description:
-    "I am a rising junior studying at University of Massachusetts Amherst graduating in Fall 2023.",
+  description: "I am a junior studying at University of Massachusetts Amherst graduating in December 2022.",
   linkedinId: "ishchhabra",
   socialLinks: [
     {
@@ -16,8 +15,7 @@ let resumeData = {
       className: "fa fa-github",
     },
   ],
-  aboutme:
-    "I am currently a rising junior majoring in Computer Science at UMass Amherst.",
+  aboutme: "I am currently a rising junior majoring in Computer Science at UMass Amherst.",
   address: "Indore, India",
   website: "https://github.com/ishchhabra",
   education: [
@@ -26,16 +24,35 @@ let resumeData = {
       specialization: "Computer Science",
       MonthOfPassing: "May",
       YearOfPassing: "2023",
-      Description: "\
+      Description:
+        "\
       Achievements:<br />\
       <ul style='list-style-type: disc; padding-left: 15px;'>\
       <li>Dean's List Fall '19</li>\
       <li>Dean's List Spring '20</li>\
       </ul>\
-      "
+      ",
     },
   ],
   work: [
+    {
+      CompanyName: "Gordian Software",
+      specialization: "Software Developer Intern (Python)",
+      MonthOfJoining: "May",
+      YearOfJoining: "2021",
+      MonthOfLeaving: "Aug",
+      YearOfLeaving: "2021",
+      Achievements:
+        "\
+      <ul style='list-style-type: disc; padding-left: 15px;'>\
+      <li>Create software and API to serve clients in the travel industry.</li>\
+      <li>Develop tests &ndash; unit testing and regression testing.</li>\
+      <li>Write web scrapers to extract flight data from different airlines.</li>\
+      <li>Develop solutions to bypass state-of-the-art anti-bot blockers by Akamai and Incapsula to facilitate web scraping.</li>\
+      <li>Develop tools to decrease development time and increase test coverage.</li>\
+      </ul>\
+      ",
+    },
     {
       CompanyName: "CICS, UMass",
       specialization: "Full Stack Developer (Intern)",
@@ -43,12 +60,15 @@ let resumeData = {
       YearOfJoining: "2020",
       MonthOfLeaving: "Aug",
       YearOfLeaving: "2020",
-      Achievements: "\
+      Achievements:
+        "\
       Project <b><a href='https://reachout.cics.umass.edu' target='_blank'>ReachOUT</a></b> <br />\
       <ul style='list-style-type: disc; padding-left: 15px;'>\
-      <li>Worked in a scrum team to design a web application for appointment scheduling using React.js, Python (AWS Lambda functions), REST API (AWS API Gateway). </li>\
-      <li>Made the app development international friendly by adapting string internationalization and time zone conversion on the frontend. </li>\
-      <li>Eliminated costs while also speeding up the application by optimizing the backend functions to reduce the data transfer between the user and the cloud by a factor of 3+. </li>\
+      <li>Integrate multiple endpoints of the application with AWS S3.</li>\
+      <li>Design, develop, and validate multiple React components to render the component.</li>\
+      <li>Write AWS Lambda functions in Python to expose API endpoints for the frontend.</li>\
+      <li>Implement string internationalization and time-zone conversion for the application to increase availability to foreign users.</li>\
+      <li>Backend Optimization to reduce data transfer between the client and the host by 3x.</li>\
       </ul>\
       ",
     },
@@ -59,14 +79,15 @@ let resumeData = {
       YearOfJoining: "2017",
       MonthOfLeaving: "Mar",
       YearOfLeaving: "2019",
-      Achievements: "\
+      Achievements:
+        "\
       <b><font color='red'>Dissolved</font></b> <br />\
       <ul style='list-style-type: disc; padding-left: 15px;'>\
-      <li>India's #1 Counter-Strike 1.6 gaming community from 2017 to 2019 according to GameTracker.com</li>\
-      <li>Designed and established several Counter-Strike 1.6 game servers hosted on AWS.</li>\
-      <li>Secured the game server by setting up the network firewall using iptables along with automated scripts using AWS SDK.</li>\
-      <li>Developed game mods in a C based scripting language called AMXMODX.</li>\
-      <li>Fostered a fun and friendly gaming environment by resolving conflicts in a community with 2000+ members.</li>\
+      <li>Ranked 1 Counter-Strike gaming community from 2017 to 2019.</li>\
+      <li>Design, develop, test, and maintain multiple Counter-Strike 1.6 servers on AWS.</li>\
+      <li>Develop the security infrastructure using AWS SDK for the servers with automated scripts to counter nefarious attacks.</li>\
+      <li>Develop game mods in a C based scripting language called AMXMODX.</li>\
+      <li>Foster a fun and friendly gaming environment in a community with 2000+ members</li>\
       </ul>\
       ",
     },
