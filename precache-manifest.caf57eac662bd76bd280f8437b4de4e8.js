4e14200de10d1c1ff7ec88b8ed03585524914872
Ish Chhabra <ichhabra@umass.edu>
2021-12-26 19:13:59 -0800

    Updates

---
+++ precache-manifest.caf57eac662bd76bd280f8437b4de4e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "632029930d1c3b98ee80",
+    "url": "/static/css/main.e7bb9fbb.chunk.css"
+  },
+  {
+    "revision": "632029930d1c3b98ee80",
+    "url": "/static/js/main.63202993.chunk.js"
+  },
+  {
+    "revision": "f0a89e02cb2c8f4d0525",
+    "url": "/static/js/1.f0a89e02.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f4c858ede35b5055f60538d711b54506",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.307b30013c85730a4e1ff69d9d19ef10.js"
+  "/precache-manifest.caf57eac662bd76bd280f8437b4de4e8.js"
 );
 
 workbox.clientsClaim();
